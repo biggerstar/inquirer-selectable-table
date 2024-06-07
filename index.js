87bfe46b4a10cecdf957aa70bc87e22eb0d0bf7b
biggerstar <1458048116@qq.com>
2024-06-08 00:21:36 +0800

    feat: 无内部边框版本

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const Base = require("inquirer/lib/prompts/base");
 const Choices = require("inquirer/lib/objects/choices");
 const observe = require("inquirer/lib/utils/events");
 const Table = require("cli-table");
-const { map, takeUntil } = require("rxjs/operators");
+const {map, takeUntil} = require("rxjs/operators");
 
-class SelecTableTablePrompt extends Base {
+class SelectTableTablePrompt extends Base {
   /**
    * Initialise the prompt
    *
@@ -37,17 +37,17 @@ class SelecTableTablePrompt extends Base {
 
     const events = observe(this.rl);
     const validation = this.handleSubmitEvents(
-      events.line.pipe(map(this.getCurrentValue.bind(this)))
+        events.line.pipe(map(this.getCurrentValue.bind(this)))
     );
     validation.success.forEach(this.onEnd.bind(this));
     validation.error.forEach(this.onError.bind(this));
 
     events.normalizedUpKey
-      .pipe(takeUntil(validation.success))
-      .forEach(this.onUpKey.bind(this));
+        .pipe(takeUntil(validation.success))
+        .forEach(this.onUpKey.bind(this));
     events.normalizedDownKey
-      .pipe(takeUntil(validation.success))
-      .forEach(this.onDownKey.bind(this));
+        .pipe(takeUntil(validation.success))
+        .forEach(this.onDownKey.bind(this));
 
     if (this.rl.line) {
       this.onKeypress();
@@ -60,7 +60,7 @@ class SelecTableTablePrompt extends Base {
   }
 
   getCurrentValue() {
-    return { ...this.rowValues[this.pointer] };
+    return {...this.rowValues[this.pointer]};
   }
 
   onDownKey() {
@@ -94,42 +94,58 @@ class SelecTableTablePrompt extends Base {
     const middleOfPage = Math.floor(this.pageSize / 2);
     const firstIndex = Math.max(0, this.pointer - middleOfPage);
     const lastIndex = Math.min(
-      firstIndex + this.pageSize - 1,
-      this.rows.realLength - 1
+        firstIndex + this.pageSize - 1,
+        this.rows.realLength - 1
     );
     const lastPageOffset = this.pageSize - 1 - lastIndex + firstIndex;
 
     return [Math.max(0, firstIndex - lastPageOffset), lastIndex];
   }
 
+  renderCallback() {
+
+  }
+
   render(error) {
     let message = this.getQuestion();
     let bottomContent = "";
 
-    if (!this.spaceKeyPressed) {
-      message +=
-        "\n (Press " +
-        chalk.cyan.bold("<space>") +
-        " to select, " +
-        chalk.cyan.bold("<Up and Down>");
-    }
+    // if (!this.spaceKeyPressed) {
+    //   message +=
+    //     "\n (Press " +
+    //     chalk.cyan.bold("<space>") +
+    //     " to select, " +
+    //     chalk.cyan.bold("<Up and Down>");
+    // }
 
     const [firstIndex, lastIndex] = this.paginate();
     const table = new Table({
+      chars: {
+        'middle': '',
+        'top-mid': '',
+        'bottom-mid': '',
+        'right-mid': '│',
+        'mid-mid': '',
+        'left-mid': '│',
+        'mid': ' ',
+      },
       head: [
-        chalk.reset.dim(
-          `${firstIndex + 1}-${lastIndex + 1} of ${this.rows.realLength}`
-        ),
+        chalk.reset.dim(``),
       ].concat(
-        this.columns.pluck("name").map((name) => chalk.reset.bold(name))
+          this.columns.pluck("name").map((name) => chalk.reset.bold(chalk.yellow(name)))
       ),
+      style: {
+        'padding-left': 1,
+        'padding-right': 1,
+      }
     });
+    this.renderCallback(table)
     this.rows.forEach((row, rowIndex) => {
       if (rowIndex < firstIndex || rowIndex > lastIndex) return;
       const chalkModifier =
-        this.status !== "answered" && this.pointer === rowIndex
-          ? chalk.reset.bold.cyan
-          : chalk.reset;
+          this.status !== "answered" && this.pointer === rowIndex
+              ? chalk.reset.bold.cyan
+              : chalk.reset;
       //  delete the inquirer name, value, short and disable before pushing
       delete row["name"];
       delete row["short"];
@@ -137,14 +153,12 @@ class SelecTableTablePrompt extends Base {
       delete row["disabled"];
 
       table.push({
-        [chalkModifier(rowIndex + 1)]: Object.values(row).map((value) =>
-          value === undefined ? "" : chalkModifier(value)
-        ),
+        ['']: Object.values(row).map((value) => value === undefined ? "" : chalkModifier(value)),
       });
     });
 
-    message += "\n\n" + table.toString();
-
+    // console.log(table.options)
+    message = table.toString()
     if (error) {
       bottomContent = chalk.red(">> ") + error;
     }
@@ -153,4 +167,4 @@ class SelecTableTablePrompt extends Base {
   }
 }
 
-module.exports = SelecTableTablePrompt;
+module.exports = SelectTableTablePrompt;
